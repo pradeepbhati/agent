8af6b8a68b7ee9d279f7ebbfd6b0082c4622e22d
Sonu Kumar <contact@tudostack.com>
2015-05-27 12:48:13 +0530

    config changes

--- scripts/config/AppConfig.js
+++ scripts/config/AppConfig.js
@@ -2,10 +2,10 @@ var Globals = {
   AppConfig : {}
 };
 (function() {
-  var chatHostBaseURI = "http://104.131.98.86";//"https://chat-staging.paytm.com";
+  var chatHostBaseURI = "http://52.24.240.75";//"https://chat-staging.paytm.com";
   Globals.AppConfig = {
   	ConcurrentChats : 3,
-  	ChatHostURI : '104.131.98.86', //chatHostBaseURI.replace(/.*?:\/\//g, ""),
+  	ChatHostURI : '52.24.240.75', //chatHostBaseURI.replace(/.*?:\/\//g, ""),
     ChatServerConnect :  chatHostBaseURI + "/accounts/connect/",
     StropheConnect : chatHostBaseURI  + "/http-bind/",
     CloseChatMessage : '{"CLSCHAT" : "chat closed"}',
